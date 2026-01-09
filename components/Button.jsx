4faa60ca604ccc8ebdff55bff10bd7abd6caa218
dollarwolv <dollarwolv@gmail.com>
2026-01-09 16:30:22 +0800

    Update Button component to include hover effects and transition for improved user interaction

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,7 +1,7 @@
 function Button({ children, className = "text-2xl" }) {
   return (
     <button
-      className={`bg-dnm-light-green rounded-full border border-black px-5 py-2 text-white shadow-[4px_4px_0px_0px_rgb(35,35,35)] ${className}`}
+      className={`bg-dnm-light-green rounded-full border border-black px-5 py-2 text-white shadow-[4px_4px_0px_0px_rgb(35,35,35)] ${className} hover:bg-dnm-black cursor-pointer transition-colors`}
     >
       {children}
     </button>
